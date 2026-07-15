50a4e64bc1993f542fcc1928079cf3783c0e7a93
Anshika Srivastava <08srivastavaanshika@gmail.com>
2026-07-15 16:40:15 +0530

    pattern 8 -> inverted pyramid

---
+++ solidPatterns/pattern8.java
@@ -0,0 +1,28 @@
+package solidPatterns;
+
+import java.util.*;
+
+public class pattern8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for(int row = 1; row<=n; row++){
+            //space + stars 
+
+            //space 
+            for(int col = 1; col <= row-1; col++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int col = 1; col<= (2*(n-row))+1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
